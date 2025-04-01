d7ae7f6ec8c609889b7090c8a46649b09095f92d
Cristobal <FHCristobal@hotmail.com>
2025-04-01 20:36:42 -0300

    Index actualizado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/techlife-app-react/', // 👈 muy importante
   build: {
     outDir: 'docs', // <--- Aquí cambias "dist" por "docs"
   },
